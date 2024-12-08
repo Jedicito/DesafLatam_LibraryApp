de051071ac6239b2fdb753b5b42218070b88da48
Jedicito <seron.cesar@gmail.com>
2024-12-08 17:44:10 -0300

    nav_graph.xml agregado

--- app/src/main/java/com/example/libraryapp/MainActivity.kt
+++ app/src/main/java/com/example/libraryapp/MainActivity.kt
@@ -5,10 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.viewbinding.ViewBinding
+import com.example.libraryapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
         setContentView(R.layout.activity_main)
 
